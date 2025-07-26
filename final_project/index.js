ab24b4b47207518b5e24037b43a80ca252c13e6f
Victoria Jury <you@example.com>
2025-07-26 11:58:40 -0400

    Updating the code for the authentication mechanism

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,25 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    try {
+        // Check headers
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1];
+
+        if (!token) {
+            return res.status(401).json({ message: "Token missing" });
+        }
+
+        // Verify JWT token
+        jwt.verify(token, "fingerprint_customer", (err, user) => {
+            if (err) return res.sendStatus(403);
+
+            req.user = user;
+            next(); // continue to route
+        });
+    } catch (err) {
+        return res.status(403).json({ message: "Invalid or expired token" });
+    }
 });
  
 const PORT =5000;
