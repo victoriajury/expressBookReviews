e2a953317f2107b590b69ff588ae3f5cedb2b770
Victoria Jury <you@example.com>
2025-07-26 15:53:48 -0400

    Updating the code with async/await endpoints

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,52 +20,145 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    if (!books || Object.keys(books).length === 0) {
-        return res.status(404).json({message: "No books found"})
+// public_users.get('/',function (req, res) {
+//     if (!books || Object.keys(books).length === 0) {
+//         return res.status(404).json({message: "No books found"})
+//     }
+//     return res.status(200).json(books)
+// });
+
+// Task 10: Get the book list available in the shop with async/await
+async function getBooksAPI() {
+    return new Promise((resolve, reject) => {
+        // simulate API call delay
+        setTimeout(() => resolve(books), 600);
+    });
+};
+
+public_users.get('/', async function (req, res) {
+    try {
+        const books_res = await getBooksAPI();
+        if (!books_res || Object.keys(books_res).length === 0) {
+            return res.status(404).json({message: "No books found"})
+        }
+        return res.status(200).json(books_res)
+
+    } catch (err) {
+        return res.status(500).json({ message: "Error in request.", error: err });
     }
-    return res.status(200).json(books)
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+// public_users.get('/isbn/:isbn',function (req, res) {
+//     try {
+//         const isbn = req.params.isbn;
+//         if (!books[isbn]) {
+//             return res.status(404).json({ message: `Book with ISBN: ${isbn} not found.` });
+//         }
+//         return res.status(200).json(books[isbn])
+    
+//     } catch (err) {
+//         return res.status(500).json({ message: "Error in request.", error: err });
+//     }
+// });
+
+// Task 11: Get book details based on ISBN with async/await
+async function getBooksByIsbnAPI(isbn) {
+    return new Promise((resolve, reject) => {
+        // simulate API call delay
+        setTimeout(() => resolve(books[isbn]), 600);
+    });
+};
+
+public_users.get('/isbn/:isbn', async function (req, res) {
     try {
         const isbn = req.params.isbn;
-        if (!books[isbn]) {
+        const books_res = await getBooksByIsbnAPI(isbn);
+        if (!books_res) {
             return res.status(404).json({ message: `Book with ISBN: ${isbn} not found.` });
         }
-        return res.status(200).json(books[isbn])
-    
+        return res.status(200).json(books_res)
+
     } catch (err) {
         return res.status(500).json({ message: "Error in request.", error: err });
     }
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+// public_users.get('/author/:author',function (req, res) {
+//     try {
+//         const author = req.params.author;
+//         const filterBooksByAuthor = Object.values(books).filter( book => book.author.toLowerCase() === author.toLocaleLowerCase())
+//         if (!filterBooksByAuthor) {
+//             return res.status(404).json({ message: `Books by author: ${author} not found.` });
+//         }
+//         return res.status(200).json(filterBooksByAuthor)
+    
+//     } catch (err) {
+//         return res.status(500).json({ message: "Error in request.", error: err });
+//     }
+// });
+
+// Task 12: Get book details based on author with async/await
+async function getBooksByAuthorAPI(author) {
+    return new Promise((resolve, reject) => {
+        // simulate API call delay
+        const filterBooksByAuthor = Object.values(books).filter(
+            book => book.author.toLowerCase() === author.toLocaleLowerCase()
+        )
+        setTimeout(() => resolve(filterBooksByAuthor), 600);
+    });
+};
+
+public_users.get('/author/:author', async function (req, res) {
     try {
         const author = req.params.author;
-        const filterBooksByAuthor = Object.values(books).filter( book => book.author.toLowerCase() === author.toLocaleLowerCase())
-        if (!filterBooksByAuthor) {
+        const books_res = await getBooksByAuthorAPI(author);
+        if (!books_res) {
             return res.status(404).json({ message: `Books by author: ${author} not found.` });
         }
-        return res.status(200).json(filterBooksByAuthor)
-    
+        return res.status(200).json(books_res)
+
     } catch (err) {
         return res.status(500).json({ message: "Error in request.", error: err });
     }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+// public_users.get('/title/:title', function (req, res) {
+//     try {
+//         const title = req.params.title;
+//         const filterBooksByTitle = Object.values(books).filter( book => book.title.toLowerCase() === title.toLocaleLowerCase())
+//         if (!filterBooksByTitle) {
+//             return res.status(404).json({ message: `Book with title: ${title} not found.` });
+//         }
+//         return res.status(200).json(filterBooksByTitle)
+    
+//     } catch (err) {
+//         return res.status(500).json({ message: "Error in request.", error: err });
+//     }
+// });
+
+// Task 13: Get all books based on title with async/await
+async function getBooksByTitleAPI(title) {
+    return new Promise((resolve, reject) => {
+        // simulate API call delay
+        const filterBooksByTitle = Object.values(books).filter( 
+            book => book.title.toLowerCase() === title.toLocaleLowerCase()
+        )        
+        setTimeout(() => resolve(filterBooksByTitle), 600);
+    });
+};
+
+public_users.get('/title/:title', async function (req, res) {
     try {
         const title = req.params.title;
-        const filterBooksByTitle = Object.values(books).filter( book => book.title.toLowerCase() === title.toLocaleLowerCase())
-        if (!filterBooksByTitle) {
+        const books_res = await getBooksByTitleAPI(title);
+        if (!books_res) {
             return res.status(404).json({ message: `Book with title: ${title} not found.` });
         }
-        return res.status(200).json(filterBooksByTitle)
-    
+        return res.status(200).json(books_res)
+
     } catch (err) {
         return res.status(500).json({ message: "Error in request.", error: err });
     }
